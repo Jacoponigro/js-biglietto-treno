5ddfdbe39c326c3a776d2ea851e192dce75740ef
Jacopo <jakonigro@gmail.com>
2020-08-04 15:38:30 +0200

    fine esercizio

--- script.js
+++ script.js
@@ -16,6 +16,5 @@ else if (anni > 65) {
 console.log(scontoVecchi)
 // calcolo prezzo biglietto
 var prezzoBiglietto = (prezzoDistanza - scontoVecchi - scontoMinorenni).toFixed(2);
-
 // stampa prezzo biglietto
 document.getElementById('prezzo').innerHTML = prezzoBiglietto + " euro";
