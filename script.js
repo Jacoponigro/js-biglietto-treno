5f57467afe47a241462542ff5dff71a2a446bb97
Jacopo <jakonigro@gmail.com>
2020-08-04 15:17:34 +0200

    esercizio finito

---
+++ script.js
@@ -0,0 +1,20 @@
+// chiedere quanti chilometri percorrere
+var distanza = prompt("quanti chilometri vuoi percorrere?");
+// chiedere quanti anni ha il passeggero
+var anni = prompt("quanti hanni hai?");
+// calcolo prezzo per distanza
+var prezzoDistanza = (distanza * 0.21);
+// calcolo sconti
+scontoVecchi = 0;
+scontoMinorenni = 0;
+if (anni < 18) {
+  var scontoMinorenni = ((prezzoDistanza *20) / 100);
+}
+else if (anni > 65) {
+  var scontoVecchi = ((prezzoDistanza *40) / 100);
+}
+console.log(scontoVecchi)
+// calcolo prezzo biglietto
+var prezzoBiglietto = prezzoDistanza - scontoVecchi - scontoMinorenni;
+// stampa prezzo biglietto
+document.getElementById('prezzo').innerHTML = prezzoBiglietto + " euro";
